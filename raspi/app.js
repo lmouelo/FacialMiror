bb4870de9ecee9cbb86ea716d105aae1a5ef86a0
Alan Guegan <alanguegan@pop-os.localdomain>
2019-02-11 10:36:51 +0100

    change to mvc

--- raspi/app.js
+++ raspi/app.js
@@ -1,42 +1,77 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require('dotenv');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+var multer = require('multer')
+var constants = require('constants');
+var constant = require('./config/constants');
 
-dotenv.config();
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+var port = process.env.PORT || 8042;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var path = require('path');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var dateFormat = require('dateformat');
+var now = new Date();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+/***************Mongodb configuratrion********************/
+var mongoose = require('mongoose');
+var configDB = require('./config/database.js');
+//configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+
+require('./config/passport')(passport); // pass passport for configuration
+
+//set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+//app.use(bodyParser()); // get information from html forms
+
+//view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs'); // set up ejs for templating
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//required for passport
+//app.use(session({ secret: 'iloveyoudear...' })); // session secret
+
+app.use(session({
+    secret: 'I Love India...',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./config/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+
+//launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
+//catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    res.status(404).render('404', {title: "Sorry, page not found", session: req.sessionbo});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (req, res, next) {
+    res.status(500).render('404', {title: "Sorry, page not found"});
 });
-
-module.exports = app;
+exports = module.exports = app;
